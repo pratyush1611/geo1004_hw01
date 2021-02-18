7c8f022c5f06b4dddbc910ebdda1b178fa09228c
Pratyush <pratyushkmr13@gmail.com>
2021-02-18 17:37:43 +0100

    added method to load obj file to memory, vertices and faces are done so far

--- main.cpp
+++ main.cpp
@@ -23,33 +23,86 @@ bool intersects(const Point &orig, const Point &dest, const Point &v0, const Poi
 
 }
 
+Point splitter(std::string str)
+{
+    // split by x y z
+    std::istringstream linestream(str);
+    std::vector<std::string> split_line
+            {
+                    std::istream_iterator<std::string>{linestream},
+                    std::istream_iterator<std::string>{}
+            };
+
+    //convert char to float
+    if (split_line.size() == 4)
+    {
+        const float x = std::stof(split_line[1]);
+        const float y = std::stof(split_line[2]);
+        const float z = std::stof(split_line[3]);
+        Point pt = Point{x, y, z};
+        return pt;
+    }
+
+}
+
+
 int main(int argc, const char * argv[])
 {
-  const char *file_in = "../bag_bk.obj";
-  const char *file_out = "vox.obj";
-  float voxel_size = 1.0;
+    const char *file_in = "E:\\TU_Delft\\yr_1\\q3\\GEO1004\\Assignment\\hw01\\hw1\\bag_bk.obj";
+    const char *file_out = "vox.obj";
+    float voxel_size = 1.0;
 
-  // Read file
-  std::vector<Point> vertices;
-  std::vector<std::vector<unsigned int>> faces;
-  // to do
+    // Read file
+    std::vector<Point> vertices;
+    std::vector<std::vector<unsigned int>> faces;
 
-  // Create grid
-  Rows rows;
-  // to do
-  VoxelGrid voxels(rows.x, rows.y, rows.z);
+    std::cout << "Reading file: " << file_in << std::endl;
+    std::ifstream infile(file_in, std::ifstream::in);
+    if(!infile)
+    {
+        std::cout<<"cant open :(\n";
+        return 1;
+    }
 
-  // Voxelise
-  for (auto const &triangle: faces)
-  {
+    std::string str;
+    while(std::getline(infile,str))
+    {
+
+        if (strnicmp(str.c_str(), "v", strlen("v") ) == 0)
+        {
+            Point vert = splitter(str);
+            vertices.push_back(vert);
+        }
+
+        else if (strnicmp(str.c_str(), "f", strlen("v") ) == 0)
+        {
+            Point vert = splitter(str);
+            unsigned int fx = static_cast<unsigned int>(vert.x);
+            unsigned int fy = static_cast<unsigned int>(vert.y);
+            unsigned int fz = static_cast<unsigned int>(vert.z);
+
+            std::vector<unsigned int> fac = {fx,fy,fz};
+            faces.push_back(fac);
+        }
+    } // end of while
+    infile.close(); //close file
+
+    // Create grid
+    Rows rows;
     // to do
-  }
+    VoxelGrid voxels(rows.x, rows.y, rows.z);
 
-  // Fill model
-  // to do
+    // Voxelise
+    for (auto const &triangle: faces)
+    {
+        // to do
+    }
 
-  // Write voxels
-  // to do
+    // Fill model
+    // to do
 
-  return 0;
+    // Write voxels
+    // to do
+
+    return 13;
 }
