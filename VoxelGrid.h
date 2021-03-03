d57df1a593e45433abb96a2b3d5414fbe8c7f17d
Ondrej Vesely <ondrej-vesely@seznam.cz>
2021-03-03 18:52:14 +0100

    Allowed Rows as Voxel indexing argument

--- VoxelGrid.h
+++ VoxelGrid.h
@@ -3,6 +3,7 @@
 
 #include "Bbox.h"
 #include "Point.h"
+#include "Rows.h"
 
 struct VoxelGrid {
   std::vector<unsigned int> voxels;
@@ -20,19 +21,28 @@ struct VoxelGrid {
     voxels.reserve(size);
     for (unsigned int i = 0; i < size; ++i) voxels.push_back(0);
   }
+
+  int _coords_to_i(const int& x, const int& y, const int& z) const {
+      assert(x >= 0 && x < max_x);
+      assert(y >= 0 && y < max_y);
+      assert(z >= 0 && z < max_z);
+      return voxels[x + y * max_x + z * max_x * max_y];
+  }
   
   unsigned int &operator()(const unsigned int &x, const unsigned int &y, const unsigned int &z) {
-    assert(x >= 0 && x < max_x);
-    assert(y >= 0 && y < max_y);
-    assert(z >= 0 && z < max_z);
-    return voxels[x + y*max_x + z*max_x*max_y];
+      return voxels[_coords_to_i(x,y,z)];
   }
   
   unsigned int operator()(const unsigned int &x, const unsigned int &y, const unsigned int &z) const {
-    assert(x >= 0 && x < max_x);
-    assert(y >= 0 && y < max_y);
-    assert(z >= 0 && z < max_z);
-    return voxels[x + y*max_x + z*max_x*max_y];
+      return voxels[_coords_to_i(x, y, z)];
+  }
+
+  unsigned int &operator()(const Rows &rows) {
+      return voxels[_coords_to_i(rows.x, rows.y, rows.z)];
+  }
+
+  unsigned int operator()(const Rows &rows) const {
+      return voxels[_coords_to_i(rows.x, rows.y, rows.z)];
   }
 
   Point center(int x, int y, int z) {
@@ -42,6 +52,7 @@ struct VoxelGrid {
           domain.min.z + (z + 1 / 2) * voxel_size
       );
   }
+
 };
 
 #endif /* VoxelGrid_h */
